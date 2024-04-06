76831f6b0ace2aae1757faa1d1afb3a26ea0840d
chandana-codes <84772049+chandana-codes@users.noreply.github.com>
2024-04-06 13:26:09 +0530

    Query

--- gcsidfarmassignment.sql
+++ gcsidfarmassignment.sql
@@ -7,6 +7,7 @@ FROM
   JOIN Routes r ON v.Vehicle_id = r.Vehicle_id
   JOIN Customers c ON r.Route_id = c.Route_id
   JOIN Orders o ON c.Customer_id = o.Customer_id
+  -- TODO: Need to implement the filter feature of delivery date between start date and end date.
 WHERE
   o.Order_status = 'confirmed'
   AND o.Delivery_date
